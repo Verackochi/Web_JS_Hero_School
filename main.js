c1a288dfbf90f7e0333bf85d11c96a327c073453
SIARHEI TSYKHUN <it4money@gmail.com>
2022-04-04 07:41:58 +0300

    v1.0.1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+<<<<<<< Updated upstream
 alert('Hello World!');
+=======
+//вывод алерта
+alert('Hello World!');
+
+// воппрос + поле для ввода данных
+let userName = prompt('Ваше имя');
+document.body.innerHTML = 'Добро Пожаловать' + userName;
+>>>>>>> Stashed changes
